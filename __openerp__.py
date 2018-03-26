c074cd70525eb6f61412747e0a54e056e3e4aedb
rolandn <roland.neyrinck@gmail.com>
2018-03-26 13:44:19 +0200

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Stock VDL",
+
+    'summary': """
+        Extention du module Stock Picking pour pouvoir laisser un commentaire sur la picking list des magasiniers.""",
+
+    'description': """
+        Le module modifie juste le module stock picking et l'information doit être encodé directement sur la picking list mais il n'est pas encore possible de l'encoder 
+        via la rédaction du bon de commande (sale_order).
+
+    """,
+
+    'author': "Roland Neyrinck",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale_stock', 'sale', 'portal_sale', 'stock_picking'],
+
+    'data': ['views/magasin_views.xml'
+             ],
+
+}
---
+++ models/magasin_model.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields
+
+class CommentaireMagasinier (models.Model):
+    _inherit = 'stock_picking'
+    commentaire = fields.Char('Commentaire pour la livraison', required=False)
