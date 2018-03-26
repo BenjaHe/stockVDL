1801173291c05bc28824a1ec2118ece087217377
rolandn <roland.neyrinck@gmail.com>
2018-03-26 14:03:46 +0200

    problème import

--- magasin_model.py
+++ magasin_model.py
@@ -3,5 +3,5 @@
 from openerp import models, fields
 
 class CommentaireMagasinier (models.Model):
-    _inherit = 'stock_picking'
+    _inherit = 'stock.picking'
     commentaire = fields.Char('Commentaire pour la livraison', required=False)
