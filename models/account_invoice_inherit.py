0bd886fe908960048b818e930a80f3831b569756
rolandn <roland.neyrinck@gmail.com>
2018-04-11 15:37:27 +0200

    budget_type

--- models/account_invoice_inherit.py
+++ models/account_invoice_inherit.py
@@ -15,5 +15,5 @@ class AccountInvoice(models.Model):
 
     payment_acquired_id_budget = fields.Many2one(comodel_name='payment.acquirer',
                                                  string='Type de budget',
-                                                 related='payment_acquirer.payment_acquirer_id',
+                                               #  related='payment_acquirer.payment_acquirer_id',
                                                  required=False)
