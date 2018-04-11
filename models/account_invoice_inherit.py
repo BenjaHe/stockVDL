57395563dd8136d4d8a281adbe07988282a58c8d
rolandn <roland.neyrinck@gmail.com>
2018-04-11 10:18:42 +0200

    budget_type

---
+++ models/account_invoice_inherit.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from operator import itemgetter
+from datetime import datetime
+
+from openerp import api, fields, models, _
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from openerp.exceptions import ValidationError
+
+
+class AccountInvoice(models.Model):
+    _name = 'account.invoice'
+    _inherit = 'account.invoice'
+    _description = 'Invoice'
+
+    payment_acquired_id = fields.Many2one('payment_acquired_id.sale.order', 'budget_type', required=False)
