28c5a7d9f0ae5670f109d04f1e991e57f8395fd6
NEYRINCKR <roland.neyrinck@gmail.com>
2019-01-11 13:25:17 +0100

    Commit pour mettre à niveau avec ajout de mrp production (fmodel et form).

---
+++ models/mrp_production_inherit.py
@@ -0,0 +1,9 @@
+from openerp import api, fields, models
+
+class mrp_production(models.Model):
+    _name = 'mrp.production'
+    _inherit = 'mrp.production'
+    _description = 'bom_production_addons'
+
+
+    description = fields.Char(string='Nom du travail', required=False, track_visibility='onchange', help='Nom du document produit.')
