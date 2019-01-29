3726ec0d2c735e26bed4b0ef87571162783b60d0
NEYRINCKR <roland.neyrinck@gmail.com>
2019-01-29 12:28:03 +0100

    erreur mic mac comit

--- models/mrp_production_inherit.py
+++ models/mrp_production_inherit.py
@@ -7,8 +7,5 @@ class mrp_production(models.Model):
 
 
     description = fields.Char(string='Nom du travail', required=False, track_visibility='onchange', help='Nom du document produit.')
-<<<<<<< HEAD
     move_lines = fields.One2many('stock.move', 'raw_material_production_id', 'Machin to Consume', readonly=False)
-=======
-    move_lines = fields.One2many('stock.move', 'raw_material_production_id', 'Machin to Consume', readonly=False)
->>>>>>> Mise à niveau de la Prod' avec le Dev du 29 janvier 2019.
+
