c4bb7ed31e6f4ea5767e63b1b06676b2c6febe14
Roland NEYRINCK <info@appalach.be>
2019-06-04 15:24:44 +0200

    ajout bouton draft pour factures

--- models/account_invoice_inherit.py
+++ models/account_invoice_inherit.py
@@ -19,9 +19,9 @@ class AccountInvoice(models.Model):
                                                  required=False)
 
 
-#  AJOUT d'un bouton pour remettre en draft les factures clients pour pouvoir les remodififer
-#     @api.one
-#     def bouton_draft(self):
-#         self.write({
-#              'state': 'draft'
-#           })
+# AJOUT d'un bouton pour remettre en draft les factures clients pour pouvoir les remodififer
+    @api.one
+    def bouton_draft(self):
+        self.write({
+             'state': 'draft'
+          })
