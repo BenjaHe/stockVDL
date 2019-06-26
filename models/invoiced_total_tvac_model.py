f85e7aaa0748b58f833724b06c3da749a717d030
Roland NEYRINCK <info@appalach.be>
2019-06-26 16:11:35 +0200

    changement info client site web

--- models/invoiced_total_tvac_model.py
+++ models/invoiced_total_tvac_model.py
@@ -259,10 +259,10 @@ class ResPartner(models.Model):
   ##          Pour afficher le reste du budget sur le web
   ###########################################################################################################""
 
-    @api.onchange('budget_CE','total_invoiced_tvac_CE_web')
+    @api.onchange('budget_CE','total_invoiced_tvac_CE_web_test')
     def _compute_budget_restant_CE_web(self):
             for partner in self:
-                partner.budget_restant_CE_web = partner.budget_CE_web - partner.total_invoiced_tvac_CE_web
+                partner.budget_restant_CE_web = partner.budget_CE - partner.total_invoiced_tvac_CE_web_test
 
             # ______________________________________________________________________________________________#
             # TOTAL_INVOICED_TVAC_CE : Calcul du total des factures TVAC sur le budget "CREDIT ECONOMAT"   #
