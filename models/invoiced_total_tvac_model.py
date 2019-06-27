e8e25de1245d8fa2b6212f2e98331d1ce93c72b3
Roland NEYRINCK <info@appalach.be>
2019-06-27 11:25:46 +0200

    changement info client site web

--- models/invoiced_total_tvac_model.py
+++ models/invoiced_total_tvac_model.py
@@ -54,7 +54,7 @@ class ResPartner(models.Model):
                                             store=False)
     total_invoiced_tvac_CE_web = fields.Char(compute='_compute_total_invoiced_tvac_CE_web',
                                                  string='Total facturé CE pour le web',
-                                                 store=True)
+                                                 store=False)
     total_invoiced_tvac_CE_web_test = fields.Monetary(compute='_compute_total_invoiced_tvac_CE_web_test',
                                                       string='Total facturé CE pour le web test',
                                                       store=False)
