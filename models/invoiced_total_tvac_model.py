3bd275bd557e3dad65b8ba11009aca130c650cae
Roland NEYRINCK <info@appalach.be>
2019-06-27 11:47:53 +0200

    changement info client site web

--- models/invoiced_total_tvac_model.py
+++ models/invoiced_total_tvac_model.py
@@ -200,7 +200,7 @@ class ResPartner(models.Model):
     def _compute_total_invoiced_tvac_CE_web(self):
             for partner in self:
                 partner.total_invoiced_tvac_CE
-                partner.total_invoiced_tvac_CE_web = "{mon_champs} {papa}".format(mon_champs=partner.total_invoiced_tvac_CE)
+                partner.total_invoiced_tvac_CE_web = "{mon_champs} papa".format(mon_champs=partner.total_invoiced_tvac_CE)
 
     @api.multi
     def _compute_total_invoiced_tvac_CE_web_test(self):
