7955d2a92c9eed526748e0489084005cc8aeeb36
Roland NEYRINCK <info@appalach.be>
2019-10-30 11:10:30 +0100

    ajout des from . import ... suite à l'erreur.

--- models/bdc.py
+++ models/bdc.py
@@ -23,7 +23,7 @@ class ResPartner(models.Model):
     mon_dyn_buyergroupid = fields.Char(string='Numéro de mon comptable dans Dynamics', required=False, related='comptable.dyn_buyergroupid',
                                     help='Le numéro du comptable dans Dynamics qui m est renseigné.')
 
-  #  dyn_orderaccount = fields.Char(string='Numéro du fournisseur dans Dynamics', required=False, help='Vient automatiquement')
+    dyn_orderaccount = fields.Char(string='Numéro du fournisseur dans Dynamics', required=False, help='Vient automatiquement')
 
 
 class Sale(models.Model):
