7767ce5b759aac757afd58396a7aa83f4712c7e8
Roland NEYRINCK <info@appalach.be>
2019-10-30 10:28:28 +0100

    Ajout du pdf sur la description des champs pour Dynamics.

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,2 @@
 # -*- coding: utf-8 -*-
 
-
-from . import invoiced_total_tvac_model
-from . import account_invoice_inherit
-from . import mrp_bom_line_inherit
-from . import mrp_bom_price
-from . import sale_inherit
-from . import bdc
-from . import mrp_production_inherit
-from . import mail_mail
-from . import purchase_inherit
-from . import finition_mrp
-from . import sale_inherit
-from . import procurement_order
-from . import purchase_order_line
