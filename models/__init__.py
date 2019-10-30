eb858b5f37eaab79ee1db0c0bab7495255ca1849
Roland NEYRINCK <info@appalach.be>
2019-10-30 10:29:42 +0100

    ajout des from . import ... suite à l'erreur.

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from . import magasin_model
+from . import invoiced_total_tvac_model
+from . import account_invoice_inherit
+from . import mrp_bom_line_inherit
+from . import mrp_bom_price
+from . import sale_inherit
+from . import bdc
+from . import mrp_production_inherit
+from . import mail_mail
+from . import purchase_inherit
+from . import finition_mrp
+from . import sale_inherit
+from . import procurement_order
+from . import purchase_order_line
